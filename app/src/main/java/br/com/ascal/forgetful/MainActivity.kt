1db130b27ce89d0cf917d15f7a0188de408a44ac
Analira Scalabrini <analira.scalabrini@gmail.com>
2018-02-07 10:30:34 -0200

    First commit

---
+++ app/src/main/java/br/com/ascal/forgetful/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.com.ascal.forgetful
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by analira on 07/02/18.
+ */
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
